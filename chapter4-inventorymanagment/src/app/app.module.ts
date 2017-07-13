e87570aee5c69ffe31bd9d2c4a60e526b09390d8
sirajuddins <sirajuddins@lumeris.com>
2017-07-13 15:37:43 -0500

    new

--- chapter4-inventorymanagment/src/app/app.module.ts
+++ chapter4-inventorymanagment/src/app/app.module.ts
@@ -7,13 +7,29 @@ import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserItemComponent } from './user-item/user-item.component';
 import { ArticleComponent } from './article/article.component';
+import { ProductImageComponent } from './product-image/product-image.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
+import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { InventoryAppComponent } from './inventory-app/inventory-app.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
     UserItemComponent,
-    ArticleComponent
+    ArticleComponent,
+    ProductImageComponent,
+    ProductDepartmentComponent,
+    PriceDisplayComponent,
+    ProductRowComponent,
+    ProductsListComponent,
+    InventoryAppComponent,
+    BreadcrumbsComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
